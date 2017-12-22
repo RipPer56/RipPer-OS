ab502797b06a7a55e707aa5d696acc59027ef6fa
RipPer56 <balbalwail@gmail.com>
2017-12-22 01:40:00 +0000

    add build files

---
+++ build.sh
@@ -0,0 +1,6 @@
+nasm -f elf32 kernel.asm -o kasm.o
+gcc -m32 -c kernel.c -o kc.o
+ld -m elf_i386 -T link.ld  -o kernel.bin  kasm.o kc.o
+cp kernel.bin iknow/boot/kernel.bin
+qemu-system-i386 -kernel kernel.bin
+grub-mkrescue -o iknow.iso iknow/
